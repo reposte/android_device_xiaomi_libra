381985eef821ad75f8cc0350f8d803b3f114ae59
Carlos Solano <csolano@gmail.com>
2016-07-12 07:52:44 +0200

    Add RR kernel resources
    Change-Id: Ia575df5b996023ebdcb827423361bd14d3f8ae32

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,9 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8992
+#TARGET_KERNEL_SOURCE := kernel/xiaomi/rr
 TARGET_KERNEL_CONFIG := blackhawk_libra_defconfig
+#TARGET_KERNEL_CONFIG := resurrection_libra_defconfig
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
