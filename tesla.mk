f222fd42ab13963c977c4d06a474fca01866da0d
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-08-19 12:58:19 +0700

    libra: Use smaller boot animation
    * For better screen estate, match to vendor boot logo
    
    Change-Id: I8ed29adc02e357e449d59fe487817853bffce460

--- tesla.mk
+++ tesla.mk
@@ -1,6 +1,6 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
